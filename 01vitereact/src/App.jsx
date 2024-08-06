b3c56a56f81248b7ab2b18e5be7396a6cf92a695
chauhanusha <ushaschauhan8@gmail.com>
2024-08-06 17:48:55 +0530

    Manipulating React Components

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import Exmaple from "./Example"
+
+function App() {
+  
+
+  return (
+    <>
+    <Exmaple/>
+    <h1>This is included in react fragmentation</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Example.jsx
@@ -0,0 +1,11 @@
+
+function Exmaple(){
+    return(
+        <>
+        <h1>Today we are learning react components</h1>
+        <p>This is a paragraph</p>
+        </>
+    )
+}
+
+export default Exmaple;
