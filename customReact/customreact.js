fd6aee721f31cbbd4024e5611010d74f85a0e447
chauhanusha <ushaschauhan8@gmail.com>
2024-08-08 21:38:26 +0530

    Creating custom React Library

---
+++ customReact/customreact.js
@@ -0,0 +1,37 @@
+//we are creating our own library to understand the working of react
+//custom render fuction
+function customRender(reactElement, container){
+    /*  without using loop
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        if(prop === 'children') continue
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+    
+}
+
+
+//this is the custom element which is designed by react to create tree
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root');
+
+//custom render function
+customRender(reactElement, mainContainer)
