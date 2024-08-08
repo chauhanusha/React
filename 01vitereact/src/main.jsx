dce0d57d8f4ce756436f405251b61aa42c5d6f60
chauhanusha <ushaschauhan8@gmail.com>
2024-08-08 21:35:59 +0530

    Various jsx in React

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+//we can add the jsx inside this main file
+function MyApp(){   //this syntax is parsed in to objects to create trees
+    return(
+        <h1>This component is inside main file</h1>
+    )
+}
+
+//to check if we can directly pass the element (it will not run because it doesn't follow the syntax of react rendering function)
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+//another way of creating object
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+//variable injection in element
+const anotherUser = "chauhanusha"
+
+//using react
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click me to visit google',
+    anotherUser   //at last we can add variables 
+)
+
 //we are rendering the app component using reactDom
 ReactDOM.createRoot(document.getElementById('root')).render(
+    //this is also allowed => MyApp(), but we should not use it
+    reactElement
 
-    <App />
+    // <App />
 )
