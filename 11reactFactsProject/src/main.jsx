9591db2b891ffb8ef44e57975b087504846e24e5
chauhanusha <ushaschauhan8@gmail.com>
2025-09-24 14:49:56 +0530

    React Facts Project

---
+++ 11reactFactsProject/src/main.jsx
@@ -0,0 +1,16 @@
+//import {  } from 'react'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+  <main>
+  <h1>Fun facts about React</h1>
+  <ul>
+    <li>was first released in 2013</li>
+    <li>was originally build by jordan walke</li>
+    <li>has well over 100k stars on github</li>
+    <li>is maintained by meta</li>
+    <li>powers thousands of enterprise apps, including mobile apps</li>
+  </ul>
+  </main>
+)
