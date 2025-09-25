46ab5ff84e0e44376bad7709f7033e8cbfe2c818
chauhanusha <ushaschauhan8@gmail.com>
2025-09-25 14:28:13 +0530

    Updating CSS

--- 11reactFactsProject/src/main.jsx
+++ 11reactFactsProject/src/main.jsx
@@ -1,9 +1,12 @@
 //import {  } from 'react'
+import './index.css'
 import { createRoot } from 'react-dom/client'
+
 
 const root = createRoot(document.getElementById("root"))
 root.render(
   <main>
+    <img src="react-logo.jpg" width= "40" alt="React Logo" />
   <h1>Fun facts about React</h1>
   <ul>
     <li>was first released in 2013</li>
