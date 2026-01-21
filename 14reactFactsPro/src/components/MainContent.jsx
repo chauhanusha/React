de0de2e6e10c2a2dd0856f0eb753928b63143521
chauhanusha <ushaschauhan8@gmail.com>
2026-01-21 12:42:53 +0530

    adding main content

--- 14reactFactsPro/src/components/MainContent.jsx
+++ 14reactFactsPro/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ export default function MainContent() {
     return (
         <main>
             <h1>Fun Facts About React</h1>
-            <ul>
+            <ul className="facts-list">
                 <li className="list-item">was first released in 2013</li>
                 <li className="list-item">was originally build by jordan walke</li>
                 <li className="list-item">has well over 100k stars on github</li>
