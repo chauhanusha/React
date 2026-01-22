7082e249ecbf2b421fb35098049a0679c8a90163
chauhanusha <ushaschauhan8@gmail.com>
2026-01-22 11:51:55 +0530

    Travel Journal Project

---
+++ 15TravelJournalPractice/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="src/image.png" alt="glob"/>
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
---
+++ 15TravelJournalPractice/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import MainCon from './MainCon'
+
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+  <MainCon/>
+)
