8cce8ba629a4c75911d905dc226bb9d517bff2fe
chauhanusha <ushaschauhan8@gmail.com>
2026-01-24 13:21:58 +0530

    props destructuring

--- 16propsReact/src/Contact.jsx
+++ 16propsReact/src/Contact.jsx
@@ -1,5 +1,9 @@
-function Contact(props) {
+//function Contact({img, name, phone, email}) => prop destructuring
+//we can have variable too so to not get confused we are not destructuring it
+
+function Contact(props) {  
     console.log(props)
+    const num = 10  //to access this variable we use {num}
     return (
         <div className="contacts">
             <article className="contact-card">
@@ -29,3 +33,15 @@ function Contact(props) {
 }
 
 export default Contact
+
+
+//object destructuring => prop destructuring is same as prop is also an object
+const person = {
+    img:"this is an image",
+    name:"reactapp",
+    phone:"234111",
+    email:"reactapp.com"
+}
+
+const {img, name} = person
+console.log(img);
--- 16propsReact/src/MainCon.jsx
+++ 16propsReact/src/MainCon.jsx
@@ -1,28 +1,29 @@
 import Contact from "./Contact"
+import cat from "./cat.png"
 
 function MainCon(){
     return(
          <div className="contacts">
             <Contact
-                img="src/cat.png"
+                img={cat}
                 name="Mr. Whiskerson"
                 phone="(212) 555-1234"
                 email="mr.whiskaz@catnap.meow"
             />
             <Contact
-                img="src/cat.png"
+                img={cat}
                 name="Fluffykins"
                 phone="(212) 555-2345"
                 email="fluff@me.com"
             />
             <Contact
-                img="src/cat.png"
+                img={cat}
                 name="Felix"
                 phone="(212) 555-4567"
                 email="thecat@hotmail.com"
             />
             <Contact 
-                img="src/cat.png"
+                img={cat}
                 name="Pumpkin"
                 phone="(0800) CAT KING"
                 email="pumpkin@scrimba.com"
