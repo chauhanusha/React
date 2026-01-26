a52038841a139574d5a3855e8d84c955c337ebcd
chauhanusha <ushaschauhan8@gmail.com>
2026-01-26 16:24:11 +0530

    key prop

--- 15TravelJournalPractice/src/MainCon.jsx
+++ 15TravelJournalPractice/src/MainCon.jsx
@@ -5,6 +5,7 @@ import entries from "./data"
 export default function MainCon() {
     const entryArr = entries.map((entry) => {
         return (<Entry
+            key={entry.id}
             img={entry.img}
             title={entry.title}
             country={entry.country}
