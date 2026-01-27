9f2ff8001b6912651a4d33591f365ac23426bf00
chauhanusha <ushaschauhan8@gmail.com>
2026-01-27 13:48:45 +0530

    adding form in main

--- 17ChefClaudeProject/src/App.jsx
+++ 17ChefClaudeProject/src/App.jsx
@@ -1,7 +1,12 @@
 import Header from "./components/Header"
+import MainCon from "./components/MainCon"
+
 function App() {
   return (
+    <>
     <Header/>
+    <MainCon/>
+    </>
   )
 }
 
---
+++ 17ChefClaudeProject/src/components/MainCon.jsx
@@ -0,0 +1,11 @@
+export default function MainCon(){
+    return (
+        <main>
+            <form className="ingredient-form">
+                <input 
+                aria-label="Add ingredient" type="text" placeholder="e.g oregano"/>
+                <button>Add ingredient</button>
+            </form>
+            </main>
+    )
+}
