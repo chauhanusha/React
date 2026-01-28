81b8eaff70124f833513684985f2321afced5cae
chauhanusha <ushaschauhan8@gmail.com>
2026-01-28 13:35:30 +0530

    mapping and adding list items

--- 17ChefClaudeProject/src/components/MainCon.jsx
+++ 17ChefClaudeProject/src/components/MainCon.jsx
@@ -1,11 +1,32 @@
 export default function MainCon(){
+    const ingredientArr = ["Chicken", "Oregano", "Tomatoes"]
+
+    //mapping over ingredent array
+    const ingredientItems = ingredientArr.map((items) => {
+        return (
+            <li key={items}>{items}</li>
+        )
+    })
+
+    function handleSubmit(event){
+        event.preventDefault()   //preventing our page from refreshing again and again
+        console.log("form is submitted")
+        const formData = new FormData(event.currentTarget)   //we are accessing the form data through dom
+        const newIngredient = formData.get("ingredient")   //getting the data that is entered in form
+        // console.log(newIngredient)
+        ingredientArr.push(newIngredient)
+        console.log(ingredientArr)
+    }
     return (
         <main>
-            <form className="ingredient-form">
+            <form onSubmit={handleSubmit} className="ingredient-form">
                 <input 
-                aria-label="Add ingredient" type="text" placeholder="e.g oregano"/>
+                aria-label="Add ingredient" type="text" placeholder="e.g oregano" name="ingredient"/>
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientItems}
+            </ul>
             </main>
     )
 }
