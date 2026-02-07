3775ff9ec34686133f04fe415143e7eaaa5d9be8
chauhanusha <ushaschauhan8@gmail.com>
2026-02-07 18:14:56 +0530

    using dynamic styling in react

--- 19PracticeProject/src/App.jsx
+++ 19PracticeProject/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 import padsData from "./pads"
-function App() {
+
+function App({darkMode}) {
   const [pads, setPads] = React.useState(padsData)
+
+  const styles = {
+    backgroundColor : darkMode ? "#222222" : "#cccccc"
+  }
+
   const buttonDataEl = pads.map((pad) => (
-    <button></button>
+    <button style={styles} key={pad.id}></button>
   ))
 
   return (
