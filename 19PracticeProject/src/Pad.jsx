64544db6282623688ed9399e21ecd76bec93cf14
chauhanusha <ushaschauhan8@gmail.com>
2026-02-08 23:22:59 +0530

    adding toggle button

--- 19PracticeProject/src/Pad.jsx
+++ 19PracticeProject/src/Pad.jsx
@@ -1,8 +1,16 @@
+import React from "react"
+
 export default function Pad(props){
+    const [on, setOn] = React.useState(props.on)
+    function toggleBut() {
+        setOn(prev => !prev)
+    }
+
     return (
         <button 
         style={{backgroundColor: props.color}}
-        className = {props.on ? "on" : undefined}>
+        className = {on ? "on" : undefined}
+        onClick={toggleBut}>
         </button>
         
     )
