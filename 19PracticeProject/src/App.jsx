78d77fe4689e94be837ed5f7836db8453bc5c502
chauhanusha <ushaschauhan8@gmail.com>
2026-02-10 19:44:18 +0530

    usind second way to update state

--- 19PracticeProject/src/App.jsx
+++ 19PracticeProject/src/App.jsx
@@ -5,8 +5,13 @@ import Pad from "./Pad"
 function App() {
   const [pads, setPads] = React.useState(padsData)
 
+  function toggle(){
+    console.log("clicked")
+  }
+
   const buttonDataEl = pads.map((pad) => (
-    <Pad key={pad.id} color={pad.color} on={pad.on}/>
+    <Pad key={pad.id} color={pad.color} on={pad.on}
+    toggle={toggle}/>
   ))
 
   return (
--- 19PracticeProject/src/Pad.jsx
+++ 19PracticeProject/src/Pad.jsx
@@ -2,15 +2,12 @@ import React from "react"
 
 export default function Pad(props){
     const [on, setOn] = React.useState(props.on)
-    function toggleBut() {
-        setOn(prev => !prev)
-    }
 
     return (
         <button 
         style={{backgroundColor: props.color}}
         className = {on ? "on" : undefined}
-        onClick={toggleBut}>
+        onClick={props.toggle}>
         </button>
         
     )
