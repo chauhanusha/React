916d660250c19b632438102133fe9c26a80209d7
chauhanusha <ushaschauhan8@gmail.com>
2026-02-24 22:28:05 +0530

    using useEffect hook

--- 18reactConcepts/src/StarWar.jsx
+++ 18reactConcepts/src/StarWar.jsx
@@ -1,18 +1,23 @@
 import React  from "react";
 
 export default function StarWar(){
-    const [starWarData, setStarWarData] = React.useState(null);
+    const [starWarData, setStarWarData] = React.useState(null)
+    const [count, setCount] = React.useState(0)
 
     
-        //console.log("Effect ran")
+       React.useEffect(function(){
+         console.log("Effect ran")
         fetch("https://swapi.dev/api/people/1")
             .then(res => res.json())
-            //.then(data => setStarWarData(data))
+            .then(data => setStarWarData(data))
+       }, [count])
      
     console.log("Rendered!")
     
     return (
         <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
             <pre>{JSON.stringify(starWarData, null, 2)}</pre>
         </div>
     )
