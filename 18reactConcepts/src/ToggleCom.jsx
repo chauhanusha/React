0108460fb83b2abc7f13d415b58ec30b009b0010
chauhanusha <ushaschauhan8@gmail.com>
2026-03-14 23:40:37 +0530

    adding button to toggle component

--- 18reactConcepts/src/ToggleCom.jsx
+++ 18reactConcepts/src/ToggleCom.jsx
@@ -2,15 +2,24 @@ import React from "react"
 
 export default function ToggleCom(){
     const [isGoingOut, setIsGoingOut] = React.useState(false)
+    const [isValid, setIsValid] = React.useState(18)
 
     function changeMind(){
         setIsGoingOut(prev => !prev)
+    }
+
+    function handleClick(){
+        setIsValid(prev => {
+            if(prev <= 18) console.log("valid")
+                else console.log("not valid")
+        })
     }
 
     return(
         <main>
             <h1>this is a toggle button</h1>
             <button onClick={changeMind}>{isGoingOut ? "yes" : "no"}</button>
+            <button onClick={handleClick}>Check</button>
         </main>
     )
 }
